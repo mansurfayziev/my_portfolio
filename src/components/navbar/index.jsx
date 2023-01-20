5613d37297f4eafcb21f94e22418e02a5bd4937c
Mansur Fayziev <114176073+mansurfayziev@users.noreply.github.com>
2023-01-20 17:57:47 +0500

    Update index.jsx

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -9,11 +9,7 @@ const Navbar = ({ state }) => {
   return (
     <Style style={state === true ? { left: "0%" } : {}}>
       <NavLink to="/" className={"name"}>
-       <img style="width:30px; height:30px; border-radius:50%;"
-
-      src="https://images.pexels.com/photos/20787/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=350"
-      alt="new"
-      /> Mansur Fayziev
+       Mansur Fayziev
       </NavLink>
       <div className="links">
         <NavLink
